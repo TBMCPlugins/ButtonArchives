cb68e52102e8661d20b5ef4c7ca9fecabc407a0f
alisolarflare <alisunderstorm@gmail.com>
2016-10-07 19:46:28 -0400

    added the FlairMe Command to the set
    I forgot to do the thing, and now I'm doing the thing

--- src/alisolarflare/flairdoors/FlairMe.java
+++ src/alisolarflare/flairdoors/FlairMe.java
@@ -1,5 +1,18 @@
 package alisolarflare.flairdoors;
 
-public class FlairMe {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class FlairMe implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		sender.sendMessage("Flairing..." + sender.getName());
+		PortalListener.playersToBeFlaired.add(sender.getName());
+		sender.sendMessage("Finished Preparation! Walk through a portal to get your flair");
+		
+		return false;
+	}
 
 }
