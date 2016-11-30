00402bd3a99c42b90646b1ceb49761bfbb460c9d
alisolarflare <alisunderstorm@gmail.com>
2016-11-30 00:31:41 -0500

    WELP, Distraction Blocker kicked in

--- src/alisolarflare/components/creativeboundaries/listeners/PlotChangeListener.java
+++ src/alisolarflare/components/creativeboundaries/listeners/PlotChangeListener.java
@@ -1,7 +1,5 @@
 package alisolarflare.components.creativeboundaries.listeners;
 
-import java.util.List;
-
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,49 +12,39 @@ import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownBlock;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
 
-import alisolarflare.components.creativeboundaries.CreativeBoundariesComponent;
+import alisolarflare.components.creativeboundaries.CreativeBoundariesAPI;
 
 public class PlotChangeListener implements Listener{
-	private boolean dickmode;
-	private List<Player> cbCreatives;
-	
-	public PlotChangeListener(CreativeBoundariesComponent module){
-		this.dickmode = module.dickmode;
-		this.cbCreatives = module.cbCreatives;
-	}
 	@EventHandler
 	public void onPlayerPlotChange(PlayerChangePlotEvent plotEvent){
 		Player player = plotEvent.getPlayer();
-		if (dickmode == false){
+		if (CreativeBoundariesAPI.getforceBoundaries() == false){
 			return;
 		}
 		if (plotEvent.getPlayer().getGameMode() != GameMode.CREATIVE){
 			return;
 		}
-		if (!(cbCreatives.contains(plotEvent.getPlayer()))){
+		if (!(CreativeBoundariesAPI.isCBCreative(player))){
 			return;
 		}
 		TownBlock tb = TownyUniverse.getTownBlock(player.getLocation());
 		if (tb == null) {
-			player.setGameMode(GameMode.SURVIVAL);
 			player.sendMessage("You have left your town boundaries!");
-			cbCreatives.remove(player);
+			CreativeBoundariesAPI.toSurvival(player);
 			return;
 		}
 		Town town;
 		try {
 			town = tb.getTown();
 		} catch (NotRegisteredException e) {
-			player.setGameMode(GameMode.SURVIVAL);
 			player.sendMessage("You have left your town boundaries!!");
-			cbCreatives.remove(player);
+			CreativeBoundariesAPI.toSurvival(player);
 			return;
 		}
 		
 		if (town == null) {
-			player.setGameMode(GameMode.SURVIVAL);
 			player.sendMessage("You have left your town boundaries!!!");
-			cbCreatives.remove(player);
+			CreativeBoundariesAPI.toSurvival(player);
 			return;
 		}
 		boolean townHasRes = false;
@@ -66,9 +54,8 @@ public class PlotChangeListener implements Listener{
 			}
 		}
 		if (!townHasRes){
-			player.setGameMode(GameMode.SURVIVAL);
 			player.sendMessage("You have left your town boundaries!!!!");
-			cbCreatives.remove(player);
+			CreativeBoundariesAPI.toSurvival(player);
 		}
 	}
 }
