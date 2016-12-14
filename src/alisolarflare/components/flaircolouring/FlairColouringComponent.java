397bff70f2f4d68b54cb9df954ca9d35dcccfd8e
alisolarflare <alisunderstorm@gmail.com>
2016-12-13 22:31:43 -0500

    attempted custom crafting recipies

--- src/alisolarflare/components/flaircolouring/FlairColouringComponent.java
+++ src/alisolarflare/components/flaircolouring/FlairColouringComponent.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.earth2me.essentials.Essentials;
 
 import alisolarflare.architecture.Component;
 import alisolarflare.components.flaircolouring.letterdyes.GetLetterDye;
+import alisolarflare.components.flaircolouring.letterdyes.LetterDye;
 import alisolarflare.components.flaircolouring.letterdyes.LetterDyeListener;
 import alisolarflare.components.flaircolouring.portals.FlairMe;
 import alisolarflare.components.flaircolouring.portals.PlayerProximityLoop;
@@ -28,5 +32,16 @@ public class FlairColouringComponent extends Component {
 		registerListener(plugin, new PortalListener(plugin, this));
 		registerListener(plugin, new PlayerProximityLoop(plugin, this));
 		registerListener(plugin, new LetterDyeListener((Essentials) Bukkit.getPluginManager().getPlugin("Essentials")));
+		
+		registerRecipies(plugin);
+	}
+	
+	public void registerRecipies(JavaPlugin plugin){
+		ItemStack testItem = LetterDye.getLetterDye();
+		ShapedRecipe testRecipie = new ShapedRecipe(testItem);
+		testRecipie.shape("***","*C*","***");
+		
+		testRecipie.setIngredient('*', Material.INK_SACK);
+		
 	}
 }
