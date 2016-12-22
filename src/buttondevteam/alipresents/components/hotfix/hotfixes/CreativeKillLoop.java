f65cd9210c9c863f7e92f1bca527b931434a9b77
alisolarflare <alisunderstorm@gmail.com>
2016-12-22 05:22:04 -0500

    Finally did the duty
    The guillotine has dropped. Contact Ali *directly* and *personally* if
    you have complaints or a valid reason to use non-survival inventory.
    (Written at 5:21 AM, I might have been salty as fuck when I made this
    decision)

--- src/buttondevteam/alipresents/components/hotfix/hotfixes/CreativeKillLoop.java
+++ src/buttondevteam/alipresents/components/hotfix/hotfixes/CreativeKillLoop.java
@@ -16,12 +16,7 @@ public class CreativeKillLoop extends BukkitRunnable implements Listener {
 
 	@Override
 	public void run() {
-		plugin.getServer().broadcastMessage("CreativeLoop");
 		for (Player player : plugin.getServer().getOnlinePlayers()){
-			player.sendMessage("Player: " + player.getName());
-			player.sendMessage("World: " + player.getWorld());
-			player.sendMessage("Gamemode : " + player.getGameMode().toString());
-			player.sendMessage("IsOP: " + player.isOp());
 			if(player.getGameMode() != GameMode.SURVIVAL && player.getWorld().getName().equalsIgnoreCase("world") && player.isOp() == false){
 				player.sendMessage("[Hotfix] Every Gamemode other than survival is disabled in the new world!");
 				player.setGameMode(GameMode.SURVIVAL);
